28edf5684aa4ea7b7013076959e54e5105c3fd15
Jim Nielsen <jimniels@gmail.com>
2016-09-27 08:34:13 -0600

    Add text & link 'code is on github' to header

--- src/scripts/components/Header.js
+++ src/scripts/components/Header.js
@@ -9,7 +9,7 @@ export default class Header extends Component {
         </h1>
     		<div className='headerTagline'>
     			<p>
-            Visualize <a href='http://sass-lang.com/documentation/Sass/Script/Functions.html#hsl_functions' target='_blank'>Sass HSL color functions</a> in real-time without compiling. Brought to you by <a href='http://jim-nielsen.com/'>Jim</a> <a href='http://twitter.com/jimniels'>Nielsen</a>.
+            Visualize <a href='http://sass-lang.com/documentation/Sass/Script/Functions.html#hsl_functions' target='_blank'>Sass HSL color functions</a> in real-time without compiling. Brought to you by <a href='http://jim-nielsen.com/'>Jim</a> <a href='http://twitter.com/jimniels'>Nielsen</a>. Code is on <a href='http://github.com/jimniels/sassme'>Github</a>.
           </p>
     		</div>
     	</header>
