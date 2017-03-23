a83032b8cdb50954be0d67f704220ea3e92a6ae0
Jim Nielsen <jimniels@gmail.com>
2017-03-23 15:49:15 -0600

    Add reference to colorme.io
    Along with flex-ing our CSS muscles a bit and updating some styles,
    this commit adds a link in the header to the wonderful colorme.io made
    by @tylergaw

--- src/scripts/components/Header.js
+++ src/scripts/components/Header.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
+import sassMeLogo from '../../img/sassme-logo.svg';
 
 export default class Header extends Component {
   render() {
     return (
       <header className='header wrapper'>
-    		<h1 className='headerLogo'>
-          SassMe
+        <h1 className='headerLogo'>
+          <img src={sassMeLogo} alt='SassMe logo' width='168' height='72' />
         </h1>
-    		<div className='headerTagline'>
-    			<p>
-            Visualize <a href='http://sass-lang.com/documentation/Sass/Script/Functions.html#hsl_functions' target='_blank'>Sass HSL color functions</a> in real-time without compiling. Brought to you by <a href='http://jim-nielsen.com/'>Jim</a> <a href='http://twitter.com/jimniels'>Nielsen</a>. Code is on <a href='http://github.com/jimniels/sassme'>Github</a>.
-          </p>
-    		</div>
-    	</header>
+        <p className='headerTagline'>
+          Visualize <a href='http://sass-lang.com/documentation/Sass/Script/Functions.html#hsl_functions' target='_blank'>Sass HSL color functions</a> in real-time without compiling. Made by <a href='http://jim-nielsen.com/'>Jim</a> <a href='http://twitter.com/jimniels'>Nielsen</a>. Code’s on <a href='http://github.com/jimniels/sassme'>Github</a>. Do the same thing but in  CSS at <a href='https://colorme.io/' target='_blank'>ColorMe.io</a>.
+        </p>
+      </header>
     );
   }
 }
